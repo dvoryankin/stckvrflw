de3668948afeaf175a2d606498afef419a6c8568
Dmitry <pchellovod@gmail.com>
2018-06-24 23:46:49 +0500

    add controller spec for show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
+
     before do
       get :index
     end
@@ -13,6 +14,20 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    it 'assigns the request to @question' do
+      get :show, params: { id: question }
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      get :show, params: { id: question }
+      expect(response).to render_template :show
     end
   end
 end
