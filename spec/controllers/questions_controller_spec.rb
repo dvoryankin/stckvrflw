76728bb91c417e86a11147e3fcb8d6461d1b5776
Dmitry <pchellovod@gmail.com>
2018-06-26 22:01:36 +0500

    add create and edit methods and tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -42,19 +42,30 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    before { get :edit, id: question }
+    let(:question) { create(:question) }
+    before { get :edit, params: {id: question } }
 
     it 'assigns the requested question to @question' do
-      expect(assigns(:question)). to eq question
+      expect(assigns(:question)).to eq question
     end
 
-    it 'renders edit viwe' do
+    it 'renders edit view' do
       expect(response).to render_template :edit
     end
+  end
 
-    describe 'POST #create' do
+  describe 'POST #create' do
+    let(:question) { create(:question) }
+    
+    context 'with valid attributes' do
+      it 'saves the new question in the database' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
     end
-
   end
 
 end
