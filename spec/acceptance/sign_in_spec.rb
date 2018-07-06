dc477e6e905c2d2bfe3f9d2d02392801d0aeef94
Dvoryankin Dmitry <dvoryankindm@gmail.com>
2018-07-06 17:54:18 +0500

    fix acceptance tests for user log in

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -12,7 +12,7 @@ feature 'User sign in', %q{
     visit new_user_session_path # '/sign_in'
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq root_path
@@ -23,9 +23,9 @@ feature 'User sign in', %q{
     visit new_user_session_path # '/sign_in'
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
+    click_on 'Log in'
 
-    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_content 'Invalid Email or password.'
     expect(current_path).to eq new_user_session_path
   end
 
