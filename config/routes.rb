1e5cd431e7d8c8e1e7b075f595c0cf6188212ad4
Dvoryankin Dmitry <dvoryankindm@gmail.com>
2018-07-06 18:37:58 +0500

    Add acceptance test for question create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root to: "questions#index"
+
   devise_for :users
 
   resources :questions do
-    resources :answers, shallow: true, only: [:new, :create]
+    resources :answers, only: [:new, :create]
   end
 
-  root to: "questions#index"
 end
