d59fe666a203873dfc895573f150042ec9afebf6
Dvoryankin Dmitry <dvoryankindm@gmail.com>
2018-07-09 17:09:41 +0500

    add acceptance helper with sign_in(user) method, add it to specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,10 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 
   config.extend ControllerMacros, type: :controller
+
+  config.include AcceptanceHelper, type: :feature
+
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
